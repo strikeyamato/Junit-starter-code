a0f66f1da33cf6d88d0fa0a7c179582d60218384
Abdeltif Azzizi <73204352+Abdeltif1@users.noreply.github.com>
2024-01-25 12:32:14 -0500

    Started Code

---
+++ src/GreetingService.java
@@ -0,0 +1,10 @@
+public class GreetingService {
+
+    public String getHelloWorld() {
+        return "Hello, World!";
+    }
+
+    public String getHelloUniverse() {
+        return "Hello, Universe!";
+    }
+}
